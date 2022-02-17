a2f170cafd15aba73d5bbd34f6c2d85630b97bd7
chenq <chenqi@sunwayworld.com>
2022-02-17 17:27:25 +0800

    任务自我调节功能开发

--- src/main/java/com/mhxh/proxyer/tcp/game/cmdfactory/RefuseGameCommandRuleConstants.java
+++ src/main/java/com/mhxh/proxyer/tcp/game/cmdfactory/RefuseGameCommandRuleConstants.java
@@ -78,6 +78,6 @@ public class RefuseGameCommandRuleConstants {
     /**
      * 已经有人领取抓鬼任务弹窗
      */
-    private static final String REFUSE_CATCH_GHOST_GET_TASK_AGAIN_POPUP_CONTENT_GBK = "队伍中已有人领取过此任务";
+    private static final String REFUSE_CATCH_GHOST_GET_TASK_AGAIN_POPUP_CONTENT_GBK = "队伍中已有队员领取过此任务";
     public static final byte[] REFUSE_CATCH_GHOST_GET_TASK_AGAIN_POPUP_CONTENT_GBK_BYTES = REFUSE_CATCH_GHOST_GET_TASK_AGAIN_POPUP_CONTENT_GBK.getBytes(Charset.forName("GBK"));
 }
